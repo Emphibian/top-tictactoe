e5c5d0c0da54a408c4d9029a17ad0f064eacf288
Emphibian <RyuuSleek@protonmail.ch>
2023-09-17 11:53:46 +0530

    Add gameboard object

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,18 @@
 let gameBoardDiv = document.querySelector(".gameboard");
 
-for (let i = 0; i < 9; i++) {
-  let gameCellDiv = document.createElement("div");
-  gameCellDiv.classList.add("game-cell");
-  gameBoardDiv.appendChild(gameCellDiv);
-}
+let gameBoard = (() => {
+  let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  return { board };
+})();
+
+gameBoard.board.forEach((row) =>
+  row.forEach(() => {
+    let gameCellDiv = document.createElement("div");
+    gameCellDiv.classList.add("game-cell");
+    gameBoardDiv.appendChild(gameCellDiv);
+  }),
+);
